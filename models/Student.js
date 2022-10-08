0f74fb49fea591fd9a45ec506f34be791fb2b662
Preethinagaraj8139 <preethinagaraj997@gmail.com>
2022-10-08 13:57:47 -0700

    first commit

---
+++ models/Student.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let studentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    lname: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    rollno: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    guide: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+  {
+    collection: "students",
+  }
+);
+
+module.exports = mongoose.model("Student", studentSchema);
